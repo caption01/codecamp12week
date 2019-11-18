c34ab65e119eee433ccf311210b7bbd7eca74209
nutchapon <nutchapon.han@gmail.com>
2019-11-18 19:31:35 +0700

    update

--- projectFackbook/faceappAPI/server.js
+++ projectFackbook/faceappAPI/server.js
@@ -1,8 +1,11 @@
 const express       =   require('express');
 const mysql         =   require('mysql2');
-const bodyParser   =   require('body-parser');
+const bodyParser    =   require('body-parser');
 const db            =   require('./models');
 const cors          =   require('cors');
+const User          =   require('./service/User')
+const Post          =   require('./service/post')
+const Comments      =   require('./service/Comments')
 const PORT          =   3000;
 
 const app = express();
@@ -10,8 +13,12 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors())
 
-db.sequelize.sync({force: true}).then(() => {
+db.sequelize.sync({force: false}).then(() => {
 
+    User(app, db)    
+    Post(app, db)    
+    Comments(app, db)
+ 
     app.listen(PORT, ()=>{
         console.log(`server is running at ${PORT}`)
     })
---
+++ projectFackbook/faceappAPI/service/User.js
@@ -0,0 +1,32 @@
+module.exports= (app, db) => {
+
+    app.get('/user', (req, res) => {
+
+        db.user.findAll({include: [db.post]})
+            .then(result => res.status(201).json(result))
+            .catch(err => res.status(404).json('cant get data'))
+            
+    })
+
+    app.get('/user/:id', (req, res) => {
+        
+        let userId =  Number(req.params.id)
+
+        db.user.findAll({
+            where: {
+                user_id: userId
+            }, include: [db.post]
+        })
+            .then(result => res.status(201).json(result))
+            .catch(err => res.status(404).json(`cant get data from user_id ${req.params.id}`))
+    })
+
+    app.post('/user', (req, res) => {
+
+        db.user.create(req.body)
+            .then(result => res.status(201).json(result))
+            .catch(err => res.status(404).json('cant create user'))
+
+    })
+
+}
